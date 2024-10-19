99746a7b4adc0d3a9931453e1651cc171a1360ac
TJPNick <TJPNick@gmail.com>
2024-10-19 14:10:34 -0400

    Init

---
+++ sketch.js
@@ -0,0 +1,37 @@
+// image source: https://visibleearth.nasa.gov/images/73884/november-blue-marble-next-generation-w-topography-and-bathymetry
+
+let y = 0.0;
+let earthTexture;
+
+function preload() {
+  earthTexture = loadImage("world.topo.bathy.200411.3x5400x2700.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  // createEasyCam(); // easycam
+}
+
+function draw() {
+  background(0);
+  noStroke();
+  directionalLight(255, 255, 255, -1, 0, -1);
+  directionalLight(255, 255, 255, -1, 0, -1);
+  directionalLight(255, 255, 255, -1, 0, -1);
+  rotateZ(radians(23.5));
+  y += 0.001;
+  rotateY(y);
+  push ()
+  texture(earthTexture);
+  sphere(200, 24, 24);
+  pop ()
+  stroke(255)
+  strokeWeight(5)
+  for(let i = 0; i<10; i++){
+    //box(random(-400,400),random(-400,400),random(-400,400))
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight, WEBGL);
+}
